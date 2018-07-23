bdfaf4dc12bd04541244ecc01c0a50eee73b929d
Igor Sinitsyn <web.gas.sin@gmail.com>
2018-07-23 23:26:36 +0300

    Chapter 4 commit

--- tests/AppBundle/Entity/DinosaurTest.php
+++ tests/AppBundle/Entity/DinosaurTest.php
@@ -2,9 +2,30 @@
 namespace Tests\AppBundle\Entity;
 
 
+use AppBundle\Entity\Dinosaur;
+use PHPUnit\Framework\TestCase;
+
 class DinosaurTest extends TestCase
 {
-    public function testThatYourComputerWorks(){
+    public function testSettingLength(){
+        $dinasaour = new Dinosaur();
+        $this->assertSame(0, $dinasaour->getLength());
 
+        $dinasaour->setLength(9);
+
+        $this->assertSame(9, $dinasaour->getLength());
+    }
+
+    public function testDinosaurIsNotShrunk(){
+        $dinosaur = new Dinosaur();
+        $dinosaur->setLength(15);
+        $this->assertGreaterThan(12, $dinosaur->getLength(), 'Did you put it in washing macine');
+    }
+
+    public function testReturnsFullSpecificationOfDinosaur(){
+        $dinosaur = new Dinosaur();
+
+        $this->assertSame('The Unknown non-carnivorous dinosaur is 0 meters long',
+            $dinosaur->getSpecification());
     }
 }
