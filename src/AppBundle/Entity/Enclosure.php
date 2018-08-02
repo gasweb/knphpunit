a28a96504923f60c48e2b33a13099410847672cc
Igor Sinitsyn <web.gas.sin@gmail.com>
2018-08-02 22:47:05 +0300

    Adds annotations

--- src/AppBundle/Entity/Enclosure.php
+++ src/AppBundle/Entity/Enclosure.php
@@ -3,11 +3,18 @@ namespace AppBundle\Entity;
 
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
 
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="enclosure")
+ */
 class Enclosure
 {
     /**
      * @var Collection
+     * @ORM\Column(targetEntity="AppBundle\Entity\Dinosaur", mappedBy="enclosure", cascade={"persist"})
+     * @ORM\Column(type="string")
      */
     private $dinosaurs;
 
